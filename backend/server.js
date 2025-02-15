86719422ca804ee3d33d58e22d0fc9317cccaa53
bartech-g <bartech.js@gmail.com>
2025-02-15 22:46:16 +0100

    add mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv")
+const mongoose = require("mongoose");
+
 const app = express();
+const port = process.env.LOCAL_PORT;
 
 dotenv.config();
 
-const port = process.env.LOCAL_PORT;
+// DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`✅ App listening on port ${port}`);
 });
